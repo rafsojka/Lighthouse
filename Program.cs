ba3cbcdb7405f6390dc323b6a2ae8248bcaed259
Rafal Sojka <rafal.sojka@gamesys.co.uk>
2016-09-14 18:02:38 +0100

    wip

--- Program.cs
+++ Program.cs
@@ -4,11 +4,13 @@ using System.Linq;
 using System.Management.Instrumentation;
 using System.Text;
 using System.Threading.Tasks;
+using System.Drawing;
 
 namespace Lighthouse
 {
     class Program
     {
+        
         static void Main(string[] args)
         {
             int n = Convert.ToInt32(Console.ReadLine());
@@ -22,21 +24,46 @@ namespace Lighthouse
 
             var maxPerSize = (n - 1)/2;
 
-            Console.WriteLine(maxPerSize);
+            // n=1, n=2
+            if (maxPerSize == 0)
+            {
+                Console.WriteLine(0);
+                return;
+            }
 
-            //for (var i = 0; i < n; ++i)
-            //{
-            //    for (var j = 0; j < n; ++j)
-            //    {
-            //        Console.Write(grid[i][j]);
-            //    }
-            //    Console.WriteLine();
-            //}
+            // n>=3
+            for (var i = 0; i < n; ++i)
+            {
+                for (var j = 0; j < n; ++j)
+                {
+                    // this is not the place we can build
+                    if(grid[i][j] !='.') continue;
+
+
+                }
+            }
+
+            //var point = new Point(1, 1);
+
+                    //Console.WriteLine(maxPerSize);
+
+                    //for (var i = 0; i < n; ++i)
+                    //{
+                    //    for (var j = 0; j < n; ++j)
+                    //    {
+                    //        Console.Write(grid[i][j]);
+                    //    }
+                    //    Console.WriteLine();
+                    //}
+            }
+
+        static List<Point> findMiddle(int n)
+        {
+            var listOfPoints = new List<Point>();
+            if (n%2 == 0)
+            {
+                
+            }
         }
-
-        //static int maxCircleSizePerGridSize(int n)
-        //{
-        //    return (n%2 != 0)
-        //}
     }
 }
